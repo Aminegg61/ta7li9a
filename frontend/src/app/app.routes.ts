282c2f8f3d4c99c2d3390810f7eb9513829554f7
Aminegg61 <amine-gg-619@outlook.com>
2026-05-12 00:47:29 +0100

    Refactor routes to include guestGuard and redirects

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
-import { Landing } from './pages/landing/landing';
 import { Auth } from './pages/auth/auth';
 import { BarberDashboard } from './pages/barber/dashboard/BarberDashboard';
 import { authGuard } from './services/guard/auth.guard'; 
 import { ClientDashboard } from './pages/client/dashboard/ClientDashboard';
-import { landingGuard } from './services/guard/landingGuard';
-export const routes: Routes = [ { path: '', component: Landing , canActivate: [landingGuard] }, // homepage
-    { path: 'auth', component: Auth },
-    { 
+import { guestGuard } from './services/guard/landingGuard';
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  
+  { 
+    path: 'auth', 
+    component: Auth, 
+    canActivate: [guestGuard] // زِد هادي هنا
+  },
+
+  { 
     path: 'barber/dashboard', 
     component: BarberDashboard, 
     canActivate: [authGuard], 
@@ -18,5 +24,5 @@ export const routes: Routes = [ { path: '', component: Landing , canActivate: [l
     component: ClientDashboard, 
     canActivate: [authGuard], 
     data: { roles: ['CLIENT'] } 
-  }
-];
+  },
+  { path: '**', redirectTo: 'auth' }
