b152f2239c8dd3f1b6303cc024d02d60af020642
Aminegg61 <amine-gg-619@outlook.com>
2026-05-11 19:02:58 +0100

    Remove email field from RequestRegister DTO
    Removed email field and its validation from RequestRegister.

--- backend/Ta7li9-app/src/main/java/com/ajemi/barber/Ta7li9_app/dto/RequestRegister.java
+++ backend/Ta7li9-app/src/main/java/com/ajemi/barber/Ta7li9_app/dto/RequestRegister.java
@@ -15,9 +15,6 @@ public class RequestRegister {
     @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @Email(message = "Invalid email format")
-    @NotBlank(message = "Email is required")
-    private String email;
     @NotBlank(message = "Role is required")
     private String role;
 
