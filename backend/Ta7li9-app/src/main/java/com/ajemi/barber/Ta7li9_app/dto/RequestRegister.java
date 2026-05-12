d4b120bd79720c49a32589bc3b52e4c5c670e112
Aminegg61 <amine-gg-619@outlook.com>
2026-05-12 01:21:38 +0100

    2137133

--- backend/Ta7li9-app/src/main/java/com/ajemi/barber/Ta7li9_app/dto/RequestRegister.java
+++ backend/Ta7li9-app/src/main/java/com/ajemi/barber/Ta7li9_app/dto/RequestRegister.java
@@ -1,6 +1,5 @@
 package com.ajemi.barber.Ta7li9_app.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
