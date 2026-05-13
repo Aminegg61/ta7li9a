a813135ee613eb7034bc10ae82183ae3fd1d1f77
Aminegg61 <amine-gg-619@outlook.com>
2026-05-13 12:17:08 +0100

    Add FooterComponent to App component imports

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// 👇 Hada s-ster l-lowel li zedna (kayjib l-Footer)
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // 👇 Hna dkhlna FooterComponent m3a RouterOutlet
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
